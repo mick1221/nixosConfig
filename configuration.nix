ddb2ab785eb4da4e9218e74d91c74a4278349b71
mick1221 <noah.mickus@gmail.com>
2026-03-24 20:35:57 -0500

    inital commit

---
+++ configuration.nix
@@ -0,0 +1,290 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Enable graphics drivers
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  #programs.hyprland.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+
+   # Install firefox.
+  programs.firefox.enable = true;
+
+  programs.steam = {
+    enable = true;
+  };
+  programs.git = {
+    enable = true;
+  };
+  programs.zsh.enable = true;
+  programs.neovim = {
+    enable = true;
+};
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nmickus = {
+    isNormalUser = true;
+    description = "Noah Mickus";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+      shell = pkgs.zsh;
+  };
+
+  home-manager.users.nmickus = {pkgs, ...}: {
+
+
+
+    programs.neovim ={
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+
+      extraPackages = with pkgs;[
+        lua-language-server
+        nil  # Nix LSP
+        nodePackages.typescript-language-server
+      ];
+
+      plugins = with pkgs.vimPlugins; [
+        # Colorscheme
+        {
+          plugin = catppuccin-nvim;
+          type = "lua";
+          config = ''
+          require("catppuccin").setup({
+          flavor = "mocha", -- latte, frappe, macchiato, mocha
+          })
+          vim.cmd.colorscheme("catppuccin")
+          '';
+        }
+
+        # File tree
+        {
+          plugin = nvim-tree-lua;
+          config = ''
+            lua require("nvim-tree").setup()
+          '';
+        }
+
+        # Fuzzy finder
+        telescope-nvim
+        plenary-nvim  # telescope dependency
+
+        # Treesitter (syntax highlighting)
+        {
+          plugin = nvim-treesitter.withAllGrammars;
+          type = "lua";
+          config = ''
+            require("nvim-treesitter.configs").setup({
+              highlight = { enable = true },
+              indent = { enable = true },
+            })
+          '';
+        }
+
+        # LSP
+        {
+          plugin = nvim-lspconfig;
+          type = "lua";
+          config = ''
+            local lsp = require("lspconfig")
+            lsp.lua_ls.setup{}
+            lsp.nil_ls.setup{}
+            lsp.tsserver.setup{}
+          '';
+        }
+
+        # Autocompletion
+        nvim-cmp
+        cmp-nvim-lsp
+        luasnip
+
+        # Status line
+        {
+          plugin = lualine-nvim;
+          type = "lua";
+          config = ''
+            require("lualine").setup()
+          '';
+        }
+      ];
+
+      extraConfig = ''
+        set number
+        set relativenumber
+        set tabstop=2
+        set shiftwidth=2
+        set expandtab
+        set scrolloff=8
+        set termguicolors
+        '';
+
+
+    };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "mick1221";
+      user.email = "noah.mickus@gmail.com";
+      init.defaultBranch = "main";
+      # Optional: use a specific SSH key for GitHub
+      core.sshCommand = "ssh -i ~/.ssh/id_ed25519";
+    };
+  };
+
+  programs.zsh ={
+    enable = true;
+    enableCompletion = true;
+    history = {
+      size = 10000;         # Number of history lines to keep in memory (HISTSIZE)
+      save = 10000;         # Number of history lines to save to the history file (SAVEHIST)
+      path = "$HOME/.zsh_history"; # Optional: specific path for the history file
+      share = true;         # Share history between different zsh sessions
+      ignoreDups = true;
+    };
+
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    SHELL = "${pkgs.zsh}/bin/zsh";
+  };
+
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "25.11";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+      pkgs.nvtopPackages.amd
+      discord
+      spotify
+      neofetch
+      logseq
+
+  ];
+
+
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
